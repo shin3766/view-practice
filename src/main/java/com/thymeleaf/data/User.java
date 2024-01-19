bffdec91baec1f0709ac6f740e0065128d410c73
Yoosup Shin <brian96shin@gmail.com>
2024-01-19 12:01:23 +0900

    init commit

---
+++ src/main/java/com/thymeleaf/data/User.java
@@ -0,0 +1,14 @@
+package com.thymeleaf.data;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
